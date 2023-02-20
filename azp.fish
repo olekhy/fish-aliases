99f695b913b09e96e244acd27df773dcb9023c4a
Alexander Hutorezki <olekhy@gmail.com>
2023-02-20 10:08:02 +0100

    fe: Add AuthZProvider alias

---
+++ azp.fish
@@ -0,0 +1,3 @@
+function azp --wraps='cd ~/i/dev/veolia/algemein/authzprovider' --description 'alias azp=cd ~/i/dev/veolia/algemein/authzprovider'
+  cd ~/i/dev/veolia/algemein/authzprovider $argv; 
+end
